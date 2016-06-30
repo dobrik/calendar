9f83e71799470b93ea806bc9aeed0fed09e8c004
dobrik <dobrik1990@gmail.com>
2016-07-01 01:28:39 +0300

    mega final profit!

--- js/main.js
+++ js/main.js
@@ -9,14 +9,18 @@ $('#sendEvent').click(function () {
     var event = $('#event');
     var desc = $('#desc');
     var place = $('#place');
-    $.get('handler.php?date='+date.val()+'&event='+event.val()+'&desc='+desc.val()+'&place='+place.val(), function(e){
-        $('#event_result').html(e);
-        setTimeout(function(){
-            $('#modal-result').modal('hide');
-        },5000)
-    });
-    $('#date').val('');
-    $('#event').val('');
-    $('#desc').val('');
-    $('#place').val('');
+    if (event.val() != '' && desc.val() != '' && place.val() != '') {
+        $.get('handler.php?date=' + date.val() + '&event=' + event.val() + '&desc=' + desc.val() + '&place=' + place.val(), function (e) {
+            $('#event_result').html(e);
+            setTimeout(function () {
+                $('#modal-result').modal('hide');
+            }, 5000)
+        });
+        $('#date').val('');
+        $('#event').val('');
+        $('#desc').val('');
+        $('#place').val('');
+    }else{
+        $('#event_result').html('Все поля надо заполнить!')
+    }
 })
